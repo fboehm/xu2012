963dfd62b320d9d0201740da1501590674a6f397
fboehm <frederick.boehm@gmail.com>
2016-03-07 16:09:03 -0600

    edits to update_k()

--- R/update_k.R
+++ R/update_k.R
@@ -1,65 +1,29 @@
 #' Update kminus or kplus vector
 #'
-#' @param k a vector of kminus or kplus values
 #' @param trinary a trinary matrix
 #' @param y data matrix
 #' @param indic indicator of whether to update kminus (-1) or kplus (1)
-#' @param shape prior shape parameter for inv gamma
-#' @param scale prior scale parameter for inv gamma
+#' @param shape prior shape parameter for gamma
+#' @param rate prior rate parameter for gamma
+#' @param alpha alpha vector of sample random effects
+#' @param mu mean vector
 #' @export
-update_k <- function(k, indic = 1, trinary, y,
-                     shape = 10, scale = 0.02,
-                     sd_jump = 0.1){
-  imax <- nrow(trinary)
-  tmax <- ncol(trinary)
-  # make a proposal kminus
-  eps <- rnorm(n = imax, mean = 0, sd = sd_jump)
-  k_pro <- k + eps
-  # prior_ratio
-  logprior_ratio <- sum(log(MCMCpack::dinvgamma(k_pro,
-                                     shape = shape,
-                                     scale = scale)) -
-    log(MCMCpack::dinvgamma(k, shape = shape, scale = scale)))
-  # lik ratio calcs
-  loglik_mat_pro <- matrix(data = 0, nrow = imax, ncol = tmax)
-  loglik_mat <- matrix(data = 0, nrow = imax, ncol = tmax)
-  # all entries of the matrices are 0, initially.
-  # we then update those entries that have trinary indicator
-  # equal to zero
+update_k <- function(indic = -1, trinary, y,
+                     shape = 10, rate = 50){
+  # define imax
+  imax <- nrow(y)
+  out <- numeric(length = imax)
   for (i in 1:imax){
-    for (t in 1:tmax){
-      if (trinary[i, t] == 0 & (y[i, t] - mu[i] - alpha[t]) *
-          indic > indic * k[i]){
-        # subtract mu[i] and alpha[t]
-        loglik_mat[i, t] <- dnorm(y[i, t] - mu[i] - alpha[t],
-                                     mean = 0, sd = sigma[i],
-                                     log = TRUE)
-        loglik_mat_pro[i, t] <- dnorm(y[i, t] - mu[i] - alpha[t],
-                                       mean = 0, sd = sigma[i],
-                                       log = TRUE)
-      }
-      if (trinary[i, t] == indic & abs(y[i, t] - mu[i] - alpha[t]) < k[i]){
-        loglik_mat[i, t] <- - log(k[i])
-        loglik_mat_pro[i, t] <- - log(k_pro[i]) # equals log(1 / k_pro[i])
-      }
-    } # end loop over t
-  } # end loop over i
-  # take sum of entries in each matrix
-
-  loglik_ratio <- sum(loglik_mat_pro - loglik_mat)
-  acc_ratio <- exp(loglik_ratio + logprior_ratio)
-  if (!is.finite(acc_ratio)){
-    out <- k
+    ind <- trinary[i, ] == indic
+    bb <- max(indic * (y[i, ] - mu[i] - alpha)[ind], 0)
+    lower <- -Inf
+    upper <- 1 / bb
+    #calculate posterior parameter values for gamma
+    shape_post <- shape + sum(ind)
+    foo <- rtrunc("rgamma", n = 1, linf = lower,
+           lsup = upper, shape = shape_post,
+           rate = rate)
+    out[i] <- 1 / foo
   }
-  if (is.finite(acc_ratio)){
-    u <- runif(n = 1, min = 0, max = 1)
-    if (u < acc_ratio){
-      out <- k_pro
-    }
-    else {
-      out <- k
-    }
-  }
-
   return(out)
 }
